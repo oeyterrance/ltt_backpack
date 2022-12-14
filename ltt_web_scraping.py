e606d01b09be26ec713f9fc50e9f4e327c7aa1eb
Terrance Oey <oey.terrance@gmail.com>
2022-12-13 20:15:42 -0500

    Updated code; LTT Store no longer reports backpack.

--- ltt_web_scraping.py
+++ ltt_web_scraping.py
@@ -10,18 +10,18 @@ import re
 
 datetime = strftime("%Y-%m-%d %H:%M:%S")
 
-URL = "https://www.lttstore.com/products/backpack"
+# URL = "https://www.lttstore.com/products/backpack"
 
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, "lxml")
+# page = requests.get(URL)
+# soup = BeautifulSoup(page.content, "lxml")
 
-quantity = re.search(r'\d+', re.search(r'inventoryQuantity\s*=\s*\d+', str(soup)).group(0))
+# quantity = re.search(r'\d+', re.search(r'inventoryQuantity\s*=\s*\d+', str(soup)).group(0))
 
-data = [quantity.group(0),datetime]
+# data = [quantity.group(0),datetime]
 
-with open('ltt_data.csv', 'a+', newline='') as write_obj:
-    csv_writer = writer(write_obj)
-    csv_writer.writerow(data)
+# with open('ltt_data.csv', 'a+', newline='') as write_obj:
+#     csv_writer = writer(write_obj)
+#     csv_writer.writerow(data)
 
 
 #########################################################################################################
@@ -41,7 +41,7 @@ with open('ltt_data_screwdriver.csv', 'a+', newline='') as write_obj:
     csv_writer.writerow(data1)
 
 print("Current Date-Time: ", datetime)
-print("LTT Backpack Quantity:", quantity.group(0))
+# print("LTT Backpack Quantity:", quantity.group(0))
 print("LTT Screwdriver Quantity:", quantity1.group(0))
 
 
